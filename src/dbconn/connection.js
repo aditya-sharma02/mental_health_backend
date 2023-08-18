928347e1088e240461bb410a551986dc99164b7f
aditya-sharma02 <adyashma786@gmail.com>
2023-08-18 23:45:09 +0530

    dotenv

--- src/dbconn/connection.js
+++ src/dbconn/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect("mongodb+srv://adyashma786:sh2rP3obT6URFdA7@cluster0.yj50dct.mongodb.net/mernproject?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URL, {
 }).then(() => {
     console.log("coonection eshtablished")
 }).catch((e) => {
